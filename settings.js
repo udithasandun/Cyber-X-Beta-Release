d59f5680b6e172a8c54e8087f7f604ee55b446fc
udithasandun <89119474+udithasandun@users.noreply.github.com>
2022-11-19 12:03:31 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,26 +36,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['0759300105'] //ur owner number
+global.ownername = "Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn" //ur owner name
+global.ytname = "YT: Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn" //ur yt chanel name
+global.socialm = "GitHub: Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn" //ur github or insta name
+global.location = "Sri Lanka, kandy" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
+global.owner = ['94759300105']
+global.ownertag = '94759300105' //ur tag number
 global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
+global.botname = 'Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn' //ur bot name
+global.ig = "https://youtu.com" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.packname = "Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn" //ur sticker watermark packname
+global.author = "Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn" //ur sticker watermark author
+global.footer = "® Udithaˢᵃ̰͚͖͕͗̏̇͢͜͜͝m̴̵ᴅᴜn"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94759300105'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
